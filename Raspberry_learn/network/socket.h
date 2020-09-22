ddb16b7f4be851cba64abe336b53974b5666aa59
tony <1090378278@qq.com.com>
2020-09-22 21:36:16 +0800

    add socket2

--- Raspberry_learn/network/socket.h
+++ Raspberry_learn/network/socket.h
@@ -1,6 +1,7 @@
 #ifndef SOCKET_H
 #define SOCKET_H
 #include <netinet/in.h>
+#include <sys/socket.h>
 #include "common.h"
 class Socket
 {
@@ -13,9 +14,27 @@ public:
     static UINT16 Cntohs(UINT16 net16bitvalue);
     static UINT32 Cntohl(UINT32 net32bitvalue);
     static UINT64  Cntohll(UINT64 net64bitvalue);
+    static INT32  CinetPton(INT32 family, const char* strptr, VOID* addrptr);
+    static const CHAR* CinetNtop(INT32 family, const VOID* addrptr, CHAR* strptr, size_t len);
 private:
 
 };
-
-
+/*
+FAMILY:AF_INET AF_INET6 AF_LOCAL AF_ROUTE AF_KEY
+TYPE:SOCK_STREAM SOCK_DGRAM SOCK_SEQPACKET SOCK_RAW
+PROTOCOL:IPPROTO_TCP IPPROTO_UDP IPPROTO_SCTP
+*/
+class Tcp
+{
+public:
+    Tcp();
+    ~Tcp();
+    INT32 Csocket(INT32 family = AF_INET, INT32 type = SOCK_STREAM, INT32 protocol = IPPROTO_TCP);
+    INT32 Cconnect(const struct sockaddr* servaddr, socklen_t addrlen);
+    INT32 Cbind(const struct sockaddr* myaddr, socklen_t addrlen);
+    
+    INT32 getsockFD();
+private:
+    INT32 sockfd;
+};
 #endif
