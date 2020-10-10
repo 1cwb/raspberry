0193d00a04ced488ce02ea871cf866e71fdf9f57
tony <1090378278@qq.com.com>
2020-10-10 17:33:25 +0800

    add tcp server/client

--- Raspberry_learn/tcpclienttest/main.cpp
+++ Raspberry_learn/tcpclienttest/main.cpp
@@ -3,6 +3,7 @@
 #include "common.h"
 #include "clog.h"
 #include "msocket.h"
+#include "cfile.h"
 int main()
 {
     Clog::getInstance()->Init(STDOUT_FILENO,LEVEL_DEBUG);
@@ -10,14 +11,24 @@ int main()
     TcpClient client;
     client.setAddr(INADDR_ANY);
     client.setFamily(AF_INET);
-    client.setPort(40960);
+    client.setPort(50001);
     if(!client.startConnect())
     {
-        LOG_ERROR("start Tcp Server failuer!");
+        LOG_ERROR("start Tcp client failuer!");
         return -1;
     }
+    else{
+        Cfile file(STDOUT_FILENO);
+    }
     CHAR* buff[512] = {0};
-    read(client.getSockfd(),buff, 512); 
-    LOG_INFO("GET MSG FROM SERVER: %s\n",buff);
+    int n = 0;
+    while(true)
+    {
+        memset(buff, 0, 512);
+        write(client.getSockfd(),"wocao!!!",sizeof("wocao!!!"));
+    n = read(client.getSockfd(),buff, 512); 
+    printf("GET MSG FROM SERVER: %s\n",buff);
+    if(n == 0)break;
+    }
     return 0;
 }
