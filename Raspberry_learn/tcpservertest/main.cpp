d6bd95d61fda318e78ecb3673b3b96520af646be
tony <1090378278@qq.com>
2020-10-31 17:33:51 +0800

    add connect class

--- Raspberry_learn/tcpservertest/main.cpp
+++ Raspberry_learn/tcpservertest/main.cpp
@@ -7,187 +7,55 @@
 #include "clog.h"
 #include "msocket.h"
 #include "errno.h"
-#include "cselect.h"
+#include "cepoll.h"
+#include "thread.h"
+
 #define MAX_FD_CLIENT 1024
+
+static VOID* epollThreadFunc(VOID* data)
+{
+
+    return NULL;
+}
+
 int main()
 {
     Clog::getInstance()->Init(STDOUT_FILENO,LEVEL_DEBUG);
     LOG_DEBUG("Now Start a TCP server\n");
     NetServer server(AF_INET6);
-    Cselect mslect, mnewselect;
-    INT32 clientfd[MAX_FD_CLIENT];
-    INT32 nclientcount = 0;
-    INT32 nready = 0;
-    INT32 connfd = 0;
-    INT32 maxfd  = 0;
-    NetTool::CsetReuseAddr(server.getsockFD(), true);
     NetTool::CsetReusePort(server.getsockFD(), true);
+    NetTool::CsetReuseAddr(server.getsockFD(),true);
     if(!server.initAddr(40960,NULL))
     {
-        LOG_ERROR("Init Server Failer!");
+        LOG_ERROR("Init addr fail!");
         return -1;
     }
+    
     if(!server.start())
     {
-        LOG_ERROR("start Tcp Server failuer!");
+        LOG_ERROR("start to run server fail!");
         return -1;
     }
-    maxfd = server.getsockFD();
-    mnewselect.fdSet(server.getsockFD(),READ_FD_EM);
-    memset(clientfd, -1, sizeof(INT32) * MAX_FD_CLIENT);
+    Thread epollThread;
+    Cepoll mepoll(0);
+    epollThread.setThreadFunc(epollThreadFunc,&mepoll);
+
+    
     while(true)
     {
-        mslect.fdSet(&mnewselect, READ_FD_EM);
-        nready = mslect.selectfd(maxfd + 1, NULL);
-        if(mslect.fdIsSet(server.getsockFD(),READ_FD_EM))
+        INT32 connectfd = server.Caccept(NULL, NULL);
+        if(connectfd < 0)
         {
-            struct sockaddr_storage clintaddr;
-            socklen_t clen = sizeof(struct sockaddr_storage);
-            memset(&clintaddr, 0, clen);
-            connfd = server.Caccept((struct sockaddr*)&clintaddr, &clen);
-            if(connfd < 0)
-            {
-                continue;
-            }
-            for(nclientcount = 0; nclientcount < MAX_FD_CLIENT; nclientcount ++)
-            {
-                LOG_INFO("nclientcount is %d",nclientcount);
-                if(clientfd[nclientcount] < 0)
-                {
-                    clientfd[nclientcount] = connfd;
-                    break;
-                }
-            }
-            if(nclientcount == MAX_FD_CLIENT)
-            {
-                LOG_WARNING("Too many clients!!");
-            }
-            mnewselect.fdSet(connfd, READ_FD_EM);
-            maxfd = connfd > maxfd ? connfd : maxfd;
-            CHAR straddr[32] = {0};
-            LOG_INFO("clinet is %s",NetTool::CinetNtop(clintaddr.ss_family, &(((struct sockaddr_in6*)(&clintaddr))->sin6_addr),straddr, 32 ));
-            INT32 n = write(connfd, "欢迎使用联发科人工智能系统>>>", sizeof("欢迎使用联发科人工智能系统>>>"));
-            if(n < 0)
-            {
-                LOG_ERROR("write date to client failer!");
-            }
-            if(--nready <= 0)
-            {
-                continue;
-            }
+            LOG_ERROR("connetc fail, continue");
+            continue;
         }
 
-        for(nclientcount = 0; nclientcount < maxfd + 1; nclientcount ++)
-        {
-            if(clientfd[nclientcount] < 0)
-            {
-                continue;
-            }
-            if(mslect.fdIsSet(clientfd[nclientcount], READ_FD_EM))
-            {
-                CHAR recvbuff[256] = {0};
-                CHAR sendbuff[256] = {0};
-                INT32 n;
-                n = read(clientfd[nclientcount], recvbuff, 256);
-                if(n == 0)
-                {
-                    close(clientfd[nclientcount]);
-                    mnewselect.fdClear(clientfd[nclientcount], READ_FD_EM);
-                    clientfd[nclientcount] = -1;
-                }
-                else
-                {
-                    if(strcmp(recvbuff,"你好\n") == 0)
-                    {
-                        n = write(clientfd[nclientcount], "MTK_AI助手: 您好,很高兴为您服务\n", sizeof("MTK_AI助手: 您好,很高兴为您服务\n"));
-                        if(n <= 0)
-                        {
-                            LOG_ERROR("write date to client failer!");
-                            close(clientfd[nclientcount]);
-                            mnewselect.fdClear(clientfd[nclientcount], READ_FD_EM);
-                            clientfd[nclientcount] = -1;
-                        }
-                    }
-                    if(strcmp(recvbuff,"1024是什么节\n") == 0)
-                    {
-                        n = write(clientfd[nclientcount], "MTK_AI助手: 屌丝节...\n", sizeof("MTK_AI助手: 屌丝节...\n"));
-                        if(n <= 0)
-                        {
-                            LOG_ERROR("write date to client failer!");
-                            close(clientfd[nclientcount]);
-                            mnewselect.fdClear(clientfd[nclientcount], READ_FD_EM);
-                            clientfd[nclientcount] = -1;
-                        }
-                    }
-                    if(strcmp(recvbuff,"??\n") == 0)
-                    {
-                        sleep(1);
-                        n = write(clientfd[nclientcount], "MTK_AI助手: emmm 程序员节\n", sizeof("MTK_AI助手: emmm 程序员节\n"));
-                        if(n <= 0)
-                        {
-                            LOG_ERROR("write date to client failer!");
-                            close(clientfd[nclientcount]);
-                            mnewselect.fdClear(clientfd[nclientcount], READ_FD_EM);
-                            clientfd[nclientcount] = -1;
-                        }
-                    }
-                    if(strcmp(recvbuff,"有什么才艺,表演一下\n") == 0)
-                    {
-                        n = write(clientfd[nclientcount], "MTK_AI助手: 我会对对联\n", sizeof("MTK_AI助手: 我会对对联\n"));
-                        if(n <= 0)
-                        {
-                            LOG_ERROR("write date to client failer!");
-                            close(clientfd[nclientcount]);
-                            mnewselect.fdClear(clientfd[nclientcount], READ_FD_EM);
-                            clientfd[nclientcount] = -1;
-                        }
-                    }
-                    if(strcmp(recvbuff,"那我出个上联\n") == 0)
-                    {
-                        n = write(clientfd[nclientcount], "MTK_AI助手: 好的,请讲\n", sizeof("MTK_AI助手: 好的,请讲\n"));
-                        if(n <= 0)
-                        {
-                            LOG_ERROR("write date to client failer!");
-                            close(clientfd[nclientcount]);
-                            mnewselect.fdClear(clientfd[nclientcount], READ_FD_EM);
-                            clientfd[nclientcount] = -1;
-                        }
-                    }
-                    if(strcmp(recvbuff,"磨刀不误砍柴工\n") == 0)
-                    {
-                        sleep(1);
-                        n = write(clientfd[nclientcount], "MTK_AI助手: 读完硕士再打工\n", sizeof("MTK_AI助手: 读完硕士再打工\n"));
-                        if(n <= 0)
-                        {
-                            LOG_ERROR("write date to client failer!");
-                            close(clientfd[nclientcount]);
-                            mnewselect.fdClear(clientfd[nclientcount], READ_FD_EM);
-                            clientfd[nclientcount] = -1;
-                        }
-                        sleep(2);
-                        n = write(clientfd[nclientcount], "MTK_AI助手: 肚子饿了,我先吃饭去了,你慢慢玩..\n", sizeof("MTK_AI助手: 肚子饿了,我先吃饭去了,你慢慢玩..\n"));
-                        if(n <= 0)
-                        {
-                            LOG_ERROR("write date to client failer!");
-                            close(clientfd[nclientcount]);
-                            mnewselect.fdClear(clientfd[nclientcount], READ_FD_EM);
-                            clientfd[nclientcount] = -1;
-                        }
-                        write(clientfd[nclientcount], "   MM    MM       TTTTTTTTTTTT    KK    kk\n", sizeof("   MM    MM       TTTTTTTTTTTT    KK    kk\n"));
-                        write(clientfd[nclientcount], " MM  MMM    MM    TTTTTTTTTTTT    KK  kk\n", sizeof(" MM  MMM    MM    TTTTTTTTTTTT    KK  kk\n"));
-                        write(clientfd[nclientcount], "MM    MM     MM       TTT         KKKk\n", sizeof("MM    MM     MM       TTT         KKKk\n"));
-                        write(clientfd[nclientcount], "MM    MM     MM       TTT         KKK\n", sizeof("MM    MM     MM       TTT         KKK\n"));
-                        write(clientfd[nclientcount], "MM    MM     MM       TTT         KK  kk\n", sizeof("MM    MM     MM       TTT         KK  kk\n"));
-                        write(clientfd[nclientcount], "MM    MM     MM       TTT         KK   kk\n", sizeof("MM    MM     MM       TTT         KK   kk\n"));
-                        write(clientfd[nclientcount], "MM    MM     MM       TTT         KK    kkk\n", sizeof("MM    MM     MM       TTT         KK    kkk\n"));
-                    }
-                }
-                if(--nready <= 0)
-                {
-                    break;
-                }
-            }
-        }
+        Connect* mconnect = new Connect(connectfd);
+        epoll_event event = {
+            
+        };
+        mepoll.CepollCtl(, connectfd, )
+        LOG_INFO("CLIENT IP is %s, port is %d",mconnect->CgetAddrStr(), mconnect->CgetPort());
     }
     return 0;
 }
